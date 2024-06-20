fa6b0377a5d1257076325e96bc2ff3694a4e1ef3
Metrologist-Vian <paramedoct@gmail.com>
2024-06-20 22:58:49 +0900

    Karplus-Strong String Synthesis Decay 조절기능 추가

--- VMath/KarplusStrong.cpp
+++ VMath/KarplusStrong.cpp
@@ -18,7 +18,7 @@ WaveFunction KarplusStrong::getNoiseBurst(double _namp, double _freq, unsigned s
     return ret;
 }
 
-WaveFunction KarplusStrong::synthesis(double _namp, double _freq, double _dura, unsigned short _srate, unsigned short _sbit)
+WaveFunction KarplusStrong::synthesis(double _namp, double _freq, double _dura, unsigned short _srate, unsigned short _sbit, short(*_decay)(WaveData, unsigned long long, unsigned long long, double), double _drate)
 {
     unsigned long long size = (unsigned long long)(_dura * (double)_srate);
 
@@ -28,15 +28,33 @@ WaveFunction KarplusStrong::synthesis(double _namp, double _freq, double _dura, 
     if (_freq > 0)
     {
         WaveData noise = getNoiseBurst(_namp, _freq, _srate, _sbit).getWaveData();
+        WaveData origin = noise;
 
         for (unsigned long long i = 0; i < size; ++i)
         {
             dat[i] = noise[i % noise.size()];
-            noise[i % noise.size()] = (noise[i % noise.size()] + noise[(i + 1) % noise.size()]) / 2.0;
+            noise[i % noise.size()] = _decay(noise, i, noise.size(), _drate);
         }
     }
 
     ret.setWaveFunction(dat, _srate, _sbit);
 
     return ret;
+}
+
+short KarplusStrong::decayMoveAverage(WaveData _data, unsigned long long _idx, unsigned long long _unit, double _drate)
+{
+    short ret = 0;
+
+    for (unsigned long long i = 0; i < (unsigned long long)_drate; ++i)
+    {
+        ret += _data[(_idx + i) % _unit];
+    }
+
+    return (short)((double)ret / _drate);
+}
+
+short KarplusStrong::decayTimeExponential(WaveData _data, unsigned long long _idx, unsigned long long _unit, double _drate)
+{
+    return _data[_idx % _unit] / _drate;
 }
--- VMath/KarplusStrong.h
+++ VMath/KarplusStrong.h
@@ -7,5 +7,8 @@ private: /* parts */
 	static WaveFunction getNoiseBurst(double _namp, double _freq, unsigned short _srate, unsigned short _sbit);
 
 public: /* public use */
-	static WaveFunction synthesis(double _namp, double _freq, double _dura, unsigned short _srate, unsigned short _sbit);
+	static WaveFunction synthesis(double _namp, double _freq, double _dura, unsigned short _srate, unsigned short _sbit, short (*_decay)(WaveData, unsigned long long, unsigned long long, double), double _drate);
+
+	static short decayMoveAverage(WaveData _data, unsigned long long _idx, unsigned long long _unit, double _drate);
+	static short decayTimeExponential(WaveData _data, unsigned long long _idx, unsigned long long _unit, double _drate);
 };
