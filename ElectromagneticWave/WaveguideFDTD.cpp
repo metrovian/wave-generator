b27b589daec2a0abe52c6b5bfd3646cf5f48c8c5
metrovian <paramedoct@gmail.com>
2024-08-20 22:58:04 +0900

    Waveguide FDTD 작업 준비

---
+++ ElectromagneticWave/WaveguideFDTD.cpp
@@ -0,0 +1,11 @@
+#include "WaveguideFDTD.h"
+
+bool WaveguideFDTD::solve(MODE _mode, double _lenx, double _leny, double _period, size_t _numx, size_t _numy)
+{
+    return false;
+}
+
+bool WaveguideFDTD::solve(MODE _mode, double _lenx, double _leny, double _period, size_t _numx, size_t _numy, size_t _numt)
+{
+    return false;
+}
--- ElectromagneticWave/WaveguideFDTD.h
+++ ElectromagneticWave/WaveguideFDTD.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "FDTD.h"
+
+class WaveguideFDTD : public FDTD
+{
+protected: /* virtual */
+	bool solve(MODE _mode, double _lenx, double _leny, double _period, size_t _numx, size_t _numy) override;
+	bool solve(MODE _mode, double _lenx, double _leny, double _period, size_t _numx, size_t _numy, size_t _numt) override;
+};
