bf00b5e16e813b775b2ea4cadd4c9edf8ddc1556
metrovian <paramedoct@gmail.com>
2024-08-31 17:28:36 +0900

    Line Scattering 작업 완료

---
+++ ElectromagneticWave/LineDDA.cpp
@@ -0,0 +1,83 @@
+#include "LineDDA.h"
+
+LineDDA::LineDDA(double _lenx, double _leny, size_t _numx, size_t _numy)
+{
+	setPlaneWave(Eigen::Vector2d(5.0, 5.0), 1.0);
+	setDipole();
+
+	lend = _lenx / 50.0;
+
+	posx = (_lenx - lend) / 2.0;
+	posy = _leny / 2.0;
+
+	double dl = lend / static_cast<double>(numd);
+
+	for (unsigned char i = 0; i < numd; ++i)
+	{
+		setDipole(Eigen::Vector2d(posx + static_cast<double>(i) * dl, posy));
+	}
+
+	solve(_lenx, _leny, _numx, _numy);
+}
+
+LineDDA::LineDDA(double _lenx, double _leny, size_t _numx, size_t _numy, double _kvex, double _kvey, double _famp)
+{
+	setPlaneWave(Eigen::Vector2d(_kvex, _kvey), _famp);
+	setDipole();
+
+	lend = _lenx / 50.0;
+
+	posx = (_lenx - lend) / 2.0;
+	posy = _leny / 2.0;
+
+	double dl = lend / static_cast<double>(numd);
+
+	for (unsigned char i = 0; i < numd; ++i)
+	{
+		setDipole(Eigen::Vector2d(posx + static_cast<double>(i) * dl, posy));
+	}
+
+	solve(_lenx, _leny, _numx, _numy);
+}
+
+LineDDA::LineDDA(double _lenx, double _leny, size_t _numx, size_t _numy, unsigned char _numd, double _lend, double _posx, double _posy)
+{
+	setPlaneWave(Eigen::Vector2d(5.0, 5.0), 1.0);
+	setDipole();
+
+	numd = _numd;
+	lend = _lend;
+
+	posx = _posx;
+	posy = _posy;
+
+	double dl = lend / static_cast<double>(numd);
+
+	for (unsigned char i = 0; i < numd; ++i)
+	{
+		setDipole(Eigen::Vector2d(posx + static_cast<double>(i) * dl, posy));
+	}
+
+	solve(_lenx, _leny, _numx, _numy);
+}
+
+LineDDA::LineDDA(double _lenx, double _leny, size_t _numx, size_t _numy, unsigned char _numd, double _lend, double _posx, double _posy, double _kvex, double _kvey, double _famp)
+{
+	setPlaneWave(Eigen::Vector2d(_kvex, _kvey), _famp);
+	setDipole();
+
+	numd = _numd;
+	lend = _lend;
+
+	posx = _posx;
+	posy = _posy;
+
+	double dl = lend / static_cast<double>(numd);
+
+	for (unsigned char i = 0; i < numd; ++i)
+	{
+		setDipole(Eigen::Vector2d(posx + static_cast<double>(i) * dl, posy));
+	}
+
+	solve(_lenx, _leny, _numx, _numy);
+}
--- ElectromagneticWave/main.cpp
+++ ElectromagneticWave/main.cpp
@@ -2,7 +2,8 @@
 #include "FreeSpaceFDTD.h"
 #include "PlanarWaveguideFDTD.h"
 #include "BraggReflectorFDTD.h"
-#include "MieDDA.h"
+#include "SquareDDA.h"
+#include "LineDDA.h"
 
 int main()
 {
@@ -15,6 +16,6 @@ int main()
     //BraggReflectorFDTD x(MODE::TRANSVERSE_ELECTRIC, 100, 100, 5.0E-7, 500, 500);
     //x.render(1000, 1000);
 
-    MieDDA s(100, 100, 500, 500);
+    LineDDA s(100, 100, 500, 500);
     s.render(1000, 1000);
 }
