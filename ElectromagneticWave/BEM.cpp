3331704dcc7ce19e9f16f0e61cc63be3c5bcfde5
metrovian <paramedoct@gmail.com>
2024-09-15 09:46:53 +0900

    BEM 클래스 작업 준비

--- ElectromagneticWave/BEM.cpp
+++ ElectromagneticWave/BEM.cpp
@@ -1,4 +1,5 @@
 #include "BEM.h"
+#include "WaveViewer.h"
 
 Element::Element()
 {
@@ -8,4 +9,34 @@ Element::Element()
 Element::Element(Eigen::Vector2d _pos)
 {
 	pos = _pos;
+}
+
+bool BEM::setElement()
+{
+	data.clear();
+	return true;
+}
+
+bool BEM::setElement(Eigen::Vector2d _pos)
+{
+	data.push_back(Element(_pos));
+	return true;
+}
+
+Eigen::MatrixXcd BEM::calcImpedanceMatrix() const
+{
+	return Eigen::MatrixXcd();
+}
+
+bool BEM::render(unsigned int _width, unsigned int _height) const
+{
+	WaveViewer viewer(_width, _height);
+	viewer.display(wave);
+
+	return true;
+}
+
+bool BEM::solve(double _lenx, double _leny, size_t _numx, size_t _numy)
+{
+	return false;
 }
--- ElectromagneticWave/BEM.h
+++ ElectromagneticWave/BEM.h
@@ -17,4 +17,17 @@ class BEM
 protected: /* data */
 	WaveField wave;
 	Antenna data;
+
+protected: /* condition */
+	bool setElement();
+	bool setElement(Eigen::Vector2d _pos);
+
+protected: /* impedance */
+	Eigen::MatrixXcd calcImpedanceMatrix() const;
+
+public: /* viewer */
+	bool render(unsigned int _width, unsigned int _height) const;
+
+protected: /* solver */
+	bool solve(double _lenx, double _leny, size_t _numx, size_t _numy);
 };
