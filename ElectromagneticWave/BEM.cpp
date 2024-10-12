8f55d61d811968d42b7b3ee5a47477e2664a9039
metrovian <paramedoct@gmail.com>
2024-10-12 23:59:15 +0900

    BEM Method of Moments Solver 작업

--- ElectromagneticWave/BEM.cpp
+++ ElectromagneticWave/BEM.cpp
@@ -1,14 +1,17 @@
 #include "BEM.h"
 #include "WaveViewer.h"
+#include "Predefined.h"
 
 Element::Element()
 {
-	pos = Eigen::Vector2d::Zero();
+	pos = Eigen::Vector2d(0.0, 0.0);
+	vec = Eigen::Vector2d(1.0, 0.0);
 }
 
-Element::Element(Eigen::Vector2d _pos)
+Element::Element(Eigen::Vector2d _pos, Eigen::Vector2d _vec)
 {
 	pos = _pos;
+	vec = _vec;
 }
 
 bool BEM::setElement()
@@ -17,9 +20,9 @@ bool BEM::setElement()
 	return true;
 }
 
-bool BEM::setElement(Eigen::Vector2d _pos)
+bool BEM::setElement(Eigen::Vector2d _pos, Eigen::Vector2d _vec)
 {
-	data.push_back(Element(_pos));
+	data.push_back(Element(_pos, _vec));
 	return true;
 }
 
@@ -33,7 +36,38 @@ bool BEM::render(unsigned int _width, unsigned int _height) const
 
 bool BEM::solve(double _lenx, double _leny, size_t _numx, size_t _numy)
 {
-	Eigen::VectorXcd cur = pinvSVD(calcImpedanceMatrix()) * calcExcitationVector();
+	if (_lenx == 0) return false;
+	if (_leny == 0) return false;
+	if (_numx == 0) return false;
+	if (_numy == 0) return false;
 
-	return false;
+	WaveField ret(MODE::TRANSVERSE_MAGNETIC, _lenx, _leny, _numx, _numy);
+
+	Eigen::Vector3cd res = Eigen::Vector3cd::Zero();
+	Eigen::VectorXcd cur = pinvSVD(calcImpedanceMatrix()) * calcExcitationVector();
+	
+	for (size_t i = 0; i < _numx; ++i)
+	{
+		for (size_t j = 0; j < _numy; ++j)
+		{
+			res = Eigen::Vector3cd::Zero();
+
+			for (size_t k = 0; k < data.size(); ++k)
+			{
+				Eigen::Vector2d pos = Eigen::Vector2d(ret.getDX() * static_cast<double>(i), ret.getDY() * static_cast<double>(j));
+
+				Eigen::Vector2d pvec = pos - data[i].pos;
+				Eigen::Vector2cd jvec = cur[i] * data[i].vec;
+
+				if (pvec.norm() > 0) res += std::complex<double>(0.0, kamp / PI / 4.0) * pvec.cross(jvec) * std::exp(std::complex<double>(0.0, -kamp * pvec.norm())) / pvec.norm();
+			}
+
+			res.x() = 0;
+			res.y() = 0;
+
+			ret.setField(res.real(), i, j);
+		}
+	}
+
+	return true;
 }
--- ElectromagneticWave/BEM.h
+++ ElectromagneticWave/BEM.h
@@ -5,11 +5,12 @@
 class Element
 {
 public: /* data */
-	Eigen::Vector2d pos = Eigen::Vector2d::Zero();
+	Eigen::Vector2d pos = Eigen::Vector2d(0.0, 0.0);
+	Eigen::Vector2d vec = Eigen::Vector2d(1.0, 0.0);
 
 public: /* constructor */
 	Element();
-	Element(Eigen::Vector2d _pos);
+	Element(Eigen::Vector2d _pos, Eigen::Vector2d _vec);
 };
 
 typedef std::vector<Element> Antenna;
@@ -19,9 +20,13 @@ protected: /* data */
 	WaveField wave;
 	Antenna data;
 
+protected: /* source */
+	double kamp = 10.0;
+	double famp = 1.0;
+
 protected: /* condition */
 	bool setElement();
-	bool setElement(Eigen::Vector2d _pos);
+	bool setElement(Eigen::Vector2d _pos, Eigen::Vector2d _vec);
 
 public: /* viewer */
 	bool render(unsigned int _width, unsigned int _height) const;
